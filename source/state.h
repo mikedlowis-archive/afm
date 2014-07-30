30fe7d79a31123424b86363f0e2e7c5e103e9765
Michael D. Lowis <mike.lowis@gentex.com>
2014-07-30 08:13:49 -0400

    Removed dead code

--- source/state.h
+++ source/state.h
@@ -15,10 +15,6 @@
 
 bool state_get_running(void);
 void state_set_running(bool val);
-//bool state_get_screen_dirty(void);
-//void state_set_screen_dirty(bool val);
-//bool state_get_screen_resized(void);
-//void state_set_screen_resized(bool val);
 bool state_get_aardvark_mode(void);
 void state_set_aardvark_mode(bool val);
 void state_set_focused_node(list_node_t* p_node);
